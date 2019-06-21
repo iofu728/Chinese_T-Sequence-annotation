12f7e193177690390960e3a1e7583a1ba9c022f7
Huiqiang Jiang <v-huji@microsoft.com>
2019-06-21 17:52:41 +0800

    😴param

---
+++ param.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: v-huji
+# @Date:   2019-06-21 10:24:51
+# @Last Modified by:   v-huji
+# @Last Modified time: 2019-06-21 10:26:01
+
+
+DATA_DIR = 'data/'
+TRAIN_FILES = f'{DATA_DIR}/Train_utf16.'
+TEST_FILES = f'{DATA_DIR}/Test'
