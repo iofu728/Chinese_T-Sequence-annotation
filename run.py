18b7ca933ab0f1f4c66635a5c66fcce6a581f515
gunjianpan <iofu728@163.com>
2019-06-23 15:51:37 +0800

    🎽entrance function

---
+++ run.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: gunjianpan
+# @Date:   2019-06-22 23:39:03
+# @Last Modified by:   gunjianpan
+# @Last Modified time: 2019-06-22 23:46:44
+
+import param
+from util import echo
+import dataLoad
+from model import Model
+
+
+def main(sa_type: param.SA_TYPE, run_id: str):
+    ''' run model '''
+    param.change_run_id(run_id)
+    echo(1, '....... Load data ......')
+    train, dev, test = dataLoad.load_data(sa_type)
+    echo(1, '....... Data load Over ......')
+    sa_model = Model(train, dev, test, sa_type)
+    echo(1, '------ Begin Train -------')
+    sa_model.run_model()
+
+
+if __name__ == "__main__":
+    main(param.SA_TYPE.CWS, 'CWS_BILSTM_CRF')
