a543ea0a601db27e869c6ed61bcd95d378c0351d
gunjianpan <iofu728@163.com>
2019-06-24 23:07:49 +0800

    🍽run bert

---
+++ run_bert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# @Author: gunjianpan
+# @Date:   2019-06-24 22:47:56
+# @Last Modified by:   gunjianpan
+# @Last Modified time: 2019-06-24 22:56:39
+
+ipython3 BERT_SA.py -- \
+    --task_name="Bert_NER" \
+    --do_lower_case=False \
+    --crf=True \
+    --do_train=False \
+    --do_eval=False \
+    --do_predict=True \
+    --data_dir=data/pkl \
+    --vocab_file=/Users/gunjianpan/Desktop/git/bert/chinese_L-12_H-768_A-12/vocab.txt \
+    --bert_config_file=/Users/gunjianpan/Desktop/git/bert/chinese_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=/Users/gunjianpan/Desktop/git/bert/chinese_L-12_H-768_A-12/bert_model.ckpt \
+    --max_seq_length=128 \
+    --train_batch_size=32 \
+    --learning_rate=2e-5 \
+    --num_train_epochs=2.0 \
+    --output_dir=./result/BERT
